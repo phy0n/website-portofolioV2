6c8962b022de7cd22cc904190e53acaf400922a0
Y0nnS <phionutsss@proton.me>
2025-07-03 19:32:37 +0700

    update to jsx

---
+++ src/app/layout.jsx
@@ -0,0 +1,27 @@
+import "../styles/globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Phy0n",
+  description: "Idk but this is Phy0n portofolio",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
