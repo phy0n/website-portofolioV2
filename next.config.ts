884ebaa4abd9e1c408791e4ac815fe6983bb5084
Y0nnS <phionutsss@proton.me>
2026-01-02 06:03:34 +0700

    update table project

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: process.cwd(),
+    root: __dirname,
   },
+  outputFileTracingRoot: __dirname,
   images: {
     remotePatterns: [
       {
